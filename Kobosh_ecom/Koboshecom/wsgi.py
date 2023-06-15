119e1e9f324fd01099a91366325967ca20254cc7
Emmanuel <nkememmanuel5505@gmail.com>
2023-06-15 02:58:35 +0100

    FRESH_START

--- Kobosh_ecom/Koboshecom/wsgi.py
+++ Kobosh_ecom/Koboshecom/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Kobosh_ecom project.
+WSGI config for Koboshecom project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Kobosh_ecom.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Koboshecom.settings")
 
 application = get_wsgi_application()

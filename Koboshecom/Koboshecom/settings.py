a019d98f0ebc958e3cf598ee233a48d21dcc78ca
Emmanuel <nkememmanuel5505@gmail.com>
2023-06-22 13:14:20 +0100

    'media_files'

--- Koboshecom/Koboshecom/settings.py
+++ Koboshecom/Koboshecom/settings.py
@@ -145,7 +145,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media/Koboshecom'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

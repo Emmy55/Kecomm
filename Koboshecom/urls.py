35eee7a2fd38ea97ac66584ceebdf54508cb8b94
Emmanuel <nkememmanuel5505@gmail.com>
2023-07-01 17:50:39 +0100

    thedb1

--- Koboshecom/urls.py
+++ Koboshecom/urls.py
@@ -31,5 +31,3 @@ urlpatterns = [
     path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
